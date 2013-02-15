ca8fc0d51c193ab3a884a090b24e4b8326d8e83b
Kevin Faaborg <zootella@zootella.com>
2013-02-15 15:57:47 -0500

    tests working

--- library/something.js
+++ library/something.js
@@ -2,4 +2,15 @@
 
 //here is an example file to get some tests working in
 
+exports.testSomething = function(test) {
+	test.expect(1);
+	test.ok(true, "this assertion should pass");
+	test.done();
+};
 
+/*
+exports.testSomethingElse = function(test) {
+	test.ok(false, "this assertion should fail");
+	test.done();
+};
+*/
