1b754962707a7452ff52a42fb3d54f18c1f55671
Kevin Faaborg <zootella@zootella.com>
2013-02-19 17:57:03 -0500

    node unit tutorial

---
+++ doubled/test/test-doubled.js
@@ -0,0 +1,11 @@
+
+// access the doubled component in the neighboring lib folder
+var doubled = require('../lib/doubled');
+
+// test the doubled.calculate() method
+exports['calculate'] = function(test) {  // nodeunit passes us the test object
+	test.equal(doubled.calculate(2), 4); // confirm you give it two, and it returns 4
+	test.done();
+};
+
+
