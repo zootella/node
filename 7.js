dbc122874ae816e86a3b81439e92f7c87be51fd6
Kevin Faaborg <zootella@zootella.com>
2013-03-28 02:23:51 -0400

    improved div

--- 7.js
+++ 7.js
@@ -28,18 +28,24 @@ exports.testDiv = function(test) {
 	var a;
 
 	a = div(10, 3);
-	test.ok(a.ans == 3);
-	test.ok(a.rem == 1);
+	test.ok(a.ans == 3 && a.rem == 1);
 	test.ok(a.rem);//has a remainder
 
 	a = div(12, 3);
-	test.ok(a.ans == 4);
-	test.ok(a.rem == 0);
+	test.ok(a.ans == 4 && a.rem == 0);
 	test.ok(!a.rem);//doesn't have a remainder
 
 	a = div(123456789, 555);
-	test.ok(a.ans == 222444);
-	test.ok(a.rem == 369);
+	test.ok(a.ans == 222444 && a.rem == 369);
+
+	a = div(789, 1);
+	test.ok(a.ans == 789 && a.rem == 0);
+
+	a = div(1, 2);
+	test.ok(a.ans == 0 && a.rem == 1);
+
+	a = div(1, 456);
+	test.ok(a.ans == 0 && a.rem == 1);
 
 	//catch errors
 	try { div("potato", 1); test.fail(); } catch (e) {}//not a number
