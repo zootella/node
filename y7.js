46933ce6afea06cdbed5118816c05cded7db49c8
Kevin Faaborg <zootella@zootella.com>
2013-04-05 17:31:27 -0400

    list notes

--- y7.js
+++ y7.js
@@ -1,9 +1,36 @@
 
 
 
+//what would it look like to make your own collections
+//all you need is a linked list with an interator that you can move forward and back on
+//and a set taht refuses duplcates
+//and a set or list that stays sorted as you add items
+//and allows removing quickly
+//you could probably do this all at once with a single List that has options like staySorted and allowDuplicates
+//and then classes which have to have methods like a.same(b) and a.sort(b)
+//also, you need a sort() method to resort in case an item has changed, and an isSorted() method to determine if sort() is necessary
 
 
 
-//use div() everywhere instead of /
+//Set, SortedSet, List, SortedList
+
+//List
+//has length, first, and last
+//add() and remove(item)
+
+//Item
+//has back() and next()
+//and a link back to the list it's in, check this
+//and isFirst() and isLast()
+//and here() or value() or something to get to what's held here
+
+//holds
+//has same() and sort()
+
+//yeah, this looks pretty good, start writing it and some tests
+//put it in list.js
+
+//also from this you'll do TwoBoots, probably
+
 
 
