0ff707e306b3b44adafabd4cf28a93c7f0ab5fa7
Kevin Faaborg <zootella@zootella.com>
2013-05-12 14:06:37 -0400

    small edits

--- state.js
+++ state.js
@@ -127,6 +127,23 @@ program.pulse.pulseAll();
 
 
 
+function error(name, note, more) {
+	return {
+		name: name,
+		note: note,
+		more: more
+	}
+}
+
+//before: throw "data";        if (e == "data");
+//after:  throw error("data"); if (e.name == "data");
+//so, only 4 characters longer
+
+//and now you can add detailed notes
+//and wrap and carry an exception you got in more, for instance
+
+//but you don't have to do the horrible java thing where each kind of exception is a separate type
+//or wrap up hashes on the fly, either
 
 
 
