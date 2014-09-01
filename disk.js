56ff37d4f2144bda282f8db7d6ed7973edccc1ce
Kevin Faaborg <zootella@zootella.com>
2014-09-01 00:18:12 -0400

    first sketch of task management

--- disk.js
+++ disk.js
@@ -93,54 +93,70 @@ function parsePath(s) {
 //given a Path
 //type, like folder or file or available
 function pathLook(p, next) {
-
 	try {
-		platformFile.stat(p, f);
-		var started = now();
-	} catch (e) { f(e); }
 
-	//you need a separate timer to call f yourself if it times out, and then if that happens, to prevent f from running later
+		checkType(p, "Path");
+		var t = now();
+		platformFile.stat(p.text, callback);
 
-	function f(e, r) {
+	} catch (e) { next(resultError(time, e)); }
+	function callback(e, s) { // Error e, file statistics s
 
-		var info = {};
-		info.request = { path: p };
-		info.result = { error: e, details: r };
-		info.duration = Duration(started);
+		var i = {};
+		i.statistics = s; // Save the complete statistics object from the platform
 
 		if (e && e.code == "ENOENT") { // Error no entry
-			info.type = "available";
+			i.type = "available";
+			next(resultDone(t, i));
 
 		} else if (e) { // Some other error
-			info.type = "error";
+			next(resultError(t, e));
 
-		} else if (r.isDirectory()) { // Folder
-			info.type = "folder";
-			info.accessed = r.atime;
-			info.modified = r.mtime;
-			info.created = r.ctime;
+		} else if (s.isDirectory()) { // Folder
+			i.type = "folder";
+			i.accessed = s.atime;
+			i.modified = s.mtime;
+			i.created = s.ctime;
+			next(resultDone(t, i));
 
-		} else if (r.isFile()) { // File
-			info.type = "file";
-			info.size = r.size; // Size
-			info.accessed = r.atime;
-			info.modified = r.mtime;
-			info.created = r.ctime;
+		} else if (s.isFile()) { // File
+			i.type = "file";
+			i.size = s.size; // Size
+			i.accessed = s.atime;
+			i.modified = s.mtime;
+			i.created = s.ctime;
+			next(resultDone(t, i));
 
 		} else { // Something else like a link or something
-			info.type = "other";
-		}
 
-		next(info);
+			i.type = "other";
+			next(resultDone(t, i));
+		}
 	}
 }
 //delete
-function pathDelete(p, next) {
-	fs.unlink(path, callback)
+function pathDelete(path, next) {
+	fs.unlink(path.text, function (e) {
+		if (e) next(e);
+		else   next();
+	});
 }
 //move
 function pathMove(source, target, next) {
 	fs.rename(oldPath, newPath, callback)
+
+
+fs.rename('/tmp/hello', '/tmp/world', function (err) {
+  if (err) throw err;
+  fs.stat('/tmp/world', function (err, stats) {
+    if (err) throw err;
+    console.log('stats: ' + JSON.stringify(stats));
+  });
+});
+
+
+
+
 }
 //open
 function pathOpen(p, next) {
@@ -266,7 +282,93 @@ exports.pathLook = pathLook;
 
 
 
+//   ____                 _ _   
+//  |  _ \ ___  ___ _   _| | |_ 
+//  | |_) / _ \/ __| | | | | __|
+//  |  _ <  __/\__ \ |_| | | |_ 
+//  |_| \_\___||___/\__,_|_|\__|
+//                              
 
+/*
+Use a Result to communicate how an asynchronous task ended
+
+t  The time when the asynchronous task was started
+e  An Error or Mistake that describes why the task didn't complete successfully, null if success
+r  Information about the successful result of the task, can be null
+*/
+
+/*
+function resultTimeout(t) { return Result(t, Mistake("timeout"), null); }
+function resultError(t, e)  { return Result(t, e, null); }
+function resultDone(t, r) { return Result(t, null, r); }
+*/
+
+function Monitor(next) {//monitor the completion of an asynchronous call
+
+	var t = now();//t start time
+
+
+	var o = {};
+
+	o.state = makeState();//a resource has state, meaning
+	o.state.close = function() {//we have to remember to close it
+		if (state.already()) return;
+	};
+	o.state.pulse = function() {//and the program will pulse it for us
+		if (t.expired(Time.timeout)) {
+			close(o);
+			next(Result(t, Mistake("timeout"), null));
+		}
+	}
+
+	o.fail = function (e) {//e error
+		if (isClosed(o)) return;
+		close(o);
+		next(Result(t, e, null));
+	}
+
+	o.done = function (a) {//a answer
+		if (isClosed(o)) return;
+		close(o);
+		next(Result(t, null, a));
+	}
+
+	return listState(o);
+};
+
+
+
+function Result(t, e, a) {
+	var o = {};
+
+	// Properties you can look at directly
+	o.duration = Duration(t);
+	o.e = e;//error
+	o.a = a;//answer
+
+	// See if this result is about a successful completion or an error
+	o.isError = function () { return o.e ? true : false; }
+	o.isDone = function () { return o.e ? false : true; }
+
+	// Call result.get() to be returned the successful outcome, or thrown the error
+	o.get = function () {
+		if (o.e) throw e;
+		else return o.a;
+	}
+
+	o.type = "Result";
+	return freeze(o);
+}
+
+exports.Monitor = Monitor;
+exports.Result = Result;
+
+
+
+
+
+
+//you need a separate timer to call f yourself if it times out, and then if that happens, to prevent f from running later
 
 
 
--- disk_test.js
+++ disk_test.js
@@ -11,6 +11,197 @@ require("./load").load("disk_test", function() { return this; });
 //and confirm you really get a file with a name that contains backslashes and dots, rather than navigation happening
 //if navigation happens, then it's even more important to have the check in open that makes sure the path of the file that was opened is the same as the path you tried to open
 
+//probably rename done(test) to testDone(test) so you can use done in the actual program
+
+
+
+
+
+
+
+
+//thinking about next and result
+//make it so that next can only get called once, if code tries to call it a second time, the call doesn't go through
+//for instance, platformFile.stat throws, the top try block catches, next gets called with an error result
+//later, node successfully finishes, and calls the callback somehow, your code somewhere stops this
+
+//you might be able to combine next(resultWhatever(time into a single thing
+//at the very top of the function, make a new local Monitor object that takes and wraps the given next function
+//when you make it, it records the starting time
+//then, whenever you want, synchronously or in callbacks, you can call methods on it like .fail(e) and .done(a)
+
+//this smart Result object also looks at the time and times itself out if necessary
+//maybe you can even give it a list of things to closeOnYes, .closeOnNo at the top, and then things close themselves
+
+//this is a really interesting design
+
+
+
+
+
+
+
+
+
+
+//   ____                 _ _   
+//  |  _ \ ___  ___ _   _| | |_ 
+//  | |_) / _ \/ __| | | | | __|
+//  |  _ <  __/\__ \ |_| | | |_ 
+//  |_| \_\___||___/\__,_|_|\__|
+//                              
+
+
+
+
+
+
+
+
+
+
+
+//given a Path
+//type, like folder or file or available
+function pathLook(path, next) {
+	var m = Monitor(next);
+	try {
+
+		checkType(path, "Path");
+		platformFile.stat(p.text, callback);
+
+	} catch (e) { m.fail(e); }
+	function callback(e, statistics) { // Error e, file statistics s
+		if (isClosed(m)) return;
+
+		var a = {};//answer object we'll put in the result
+		a.statistics = statistics; // Save the complete statistics object from the platform
+
+		if (e && e.code == "ENOENT") { // Error no entry
+			a.type = "available";
+			m.done(a);
+
+		} else if (e) { // Some other error
+			m.fail(e);
+
+		} else if (s.isDirectory()) { // Folder
+			a.type = "folder";
+			a.accessed = s.atime;
+			a.modified = s.mtime;
+			a.created = s.ctime;
+			m.done(a);
+
+		} else if (s.isFile()) { // File
+			a.type = "file";
+			a.size = s.size; // Size
+			a.accessed = s.atime;
+			a.modified = s.mtime;
+			a.created = s.ctime;
+			m.done(a);
+
+		} else { // Something else like a link or something
+
+			a.type = "other";
+			m.done(a);
+		}
+	}
+}
+
+
+
+function pathDelete(path, next) {
+	var m = Monitor(next);
+	try {
+
+		//before
+		checkType(path, "Path");
+		platformFile.unlink(path.text, callback);
+		//
+
+	} catch (e) { m.fail(e); }
+	function callback(e, statistics) { // Error e, file statistics s
+		if (isClosed(m)) return;
+
+		//after
+		if (e) m.fail(e);
+		else m.done();
+		//
+	}
+}
+
+
+
+//ok, to be really crazy, what you woudl do now is have two functions, before and after
+//and then have monitor take next, before, after, and do the whole thing
+//ok, here's why to not do that
+//because in its current state, it's pretty clean but it still looks like node examples, which is good
+//becuase you have the ability to easily access parameters in both before and after, and make local variables that both can get to, which is good
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.testResult = function(test) {
+
+	var t = now();
+
+	//timeout
+	var r = Result(t, Mistake("timeout"), null);
+	test.ok(r.isError());//binary outcome
+	test.ok(!r.isDone());
+	test.ok(r.e);//contained exception
+	test.ok(r.e.name == "timeout");
+	test.ok(!r.result);//no result
+	try {
+		r.get();//get throws
+		test.fail();
+	} catch (e) { test.ok(r.e.name == "timeout"); }
+
+	//mistake
+	r = Result(t, Mistake("data"), null);
+	test.ok(r.isError());//binary outcome
+	test.ok(!r.isDone());
+	test.ok(r.e);//contained exception
+	test.ok(r.e.name == "data");
+	test.ok(!r.result);//no result
+	try {
+		r.get();//get throws
+		test.fail();
+	} catch (e) { test.ok(r.e.name == "data"); }
+
+	//success with no details
+	r = Result(t, null, null);
+	test.ok(!r.isError());//binary outcome
+	test.ok(r.isDone());
+	test.ok(!r.e);//contained exception
+	test.ok(!r.result);//result not required
+	test.ok(!r.get());//get returns undefined, but doesn't throw
+
+	//success with details
+	r = Result(t, null, {color:"blue"});
+	test.ok(!r.isError());//binary outcome
+	test.ok(r.isDone());
+	test.ok(!r.e);//contained exception
+	test.ok(r.result);
+	test.ok(r.result.color == "blue");
+	test.ok(r.get().color == "blue");//get doesn't throw
+
+	done(test);
+}
+
+
+
+
+
+
 
 
 
